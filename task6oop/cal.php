6c981f30a398429f5dc92104a9b8b6d03f534b7b
Mostafa Hesham <mostafahesham5500@gmail.com>
2022-04-25 04:02:15 +0200

    m

---
+++ task6oop/cal.php
@@ -0,0 +1,14 @@
+<?php 
+    
+    class calc{
+        function rect($len,$dis){
+            return $len * $dis;
+        }
+        function angle($r){
+            return  * $r *$r;
+        }
+    }
+
+    $result = new calc();
+
+?>
